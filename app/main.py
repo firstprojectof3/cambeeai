84f3538f8e5c4ae94fc55b3ebdbe0ce452e2c092
이승현 <108600956+teumYee@users.noreply.github.com>
2025-08-17 09:45:05 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,29 +2,43 @@
 # FAST API AI 앱 실행, 라우팅 처리
 from fastapi import FastAPI
 from app.models.chat import ChatRequest, ChatResponse
-from openai import OpenAI 
+# from openai import OpenAI 
 from datetime import datetime 
 
 from app.services.ai.prompt.prompt_builder import build_generic_prompt
 from app.models.user_db import get_user_by_id
-from app.services.ai.ai_setting import call_openai
+from app.services.ai.ai_setting import call_openai,client
 
 import os
 from dotenv import load_dotenv
+
+import openai
+from fastapi.middleware.cors import CORSMiddleware
+
 
 load_dotenv()
 
 app = FastAPI()
 
-api_key=os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=api_key)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 또는 ["http://localhost:3000"] 등 프론트엔드 주소
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+openai.api_key=os.getenv("OPENAI_API_KEY")
+# client = OpenAI(api_key=api_key)
 
 @app.post("/chat", response_model=ChatResponse)
 def chat(req: ChatRequest):
     user = get_user_by_id(req.user_id)
     prompt = build_generic_prompt(user, req.message)
     
-    response = call_openai(client, [
+    response = call_openai(client,[
         {"role": "system", "content": prompt},
         {"role": "user", "content": req.message}
     ])
